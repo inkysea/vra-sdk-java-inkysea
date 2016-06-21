6b58e5ce3c8eee5371b77807ae413fd14dd0986d
kthieler <Only4github>
2016-06-21 14:37:26 -0400

    First Commit!

---
+++ src/main/java/com/inkysea/vmware/vra/sdk/RestClient.java
@@ -0,0 +1,32 @@
+package com.inkysea.vmware.vra.sdk;
+
+import org.apache.http.HttpResponse;
+
+/**
+ * Created by kthieler on 6/21/16.
+ */
+public class RestClient {
+    public void RestClient(){
+
+    }
+    public int dummy(){
+        return 1;
+    }
+
+    public String AuthToken(){
+        return null;
+    }
+
+    public HttpResponse Get(){
+        return null;
+    }
+
+    public HttpResponse Post(){
+        return null;
+    }
+
+    public HttpResponse Put(){
+        return null;
+    }
+
+}
---
+++ test/java/com/inkysea/vmware/vra/sdk/RestClientTest.java
@@ -0,0 +1,51 @@
+package com.inkysea.vmware.vra.sdk;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kthieler on 6/21/16.
+ */
+public class RestClientTest {
+
+
+    @Test
+    public void dummy() throws Exception {
+        RestClient test = new RestClient ();
+        test.dummy();
+        assertEquals("Dummy must return 1", 1, test.dummy());
+    }
+
+    @Test
+    public void test_auth(){
+        RestClient test = new RestClient ();
+        assertNotNull("Token must not be null", test.AuthToken());
+    }
+
+    @Test
+    public void test_get(){
+        RestClient test = new RestClient ();
+        test.AuthToken();
+        test.Get();
+        assertNotNull("Token must not be null", test.Get());
+    }
+
+    @Test
+    public void test_post(){
+        RestClient test = new RestClient ();
+        test.AuthToken();
+        test.Post();
+        assertNotNull("Token must not be null", test.Post());
+    }
+
+    @Test
+    public void test_put(){
+        RestClient test = new RestClient ();
+        test.AuthToken();
+        test.Put();
+        assertNotNull("Token must not be null", test.Put());
+    }
+
+
+}
