312bc36bb3405afb903dfec44ce2215f3c919859
kthieler <Only4github>
2016-08-22 10:33:02 -0400

    VMworld 2016!!!

--- src/test/java/com/inkysea/vmware/vra/sdk/RestClientTest.java
+++ src/test/java/com/inkysea/vmware/vra/sdk/RestClientTest.java
@@ -142,7 +142,7 @@ public class RestClientTest {
 
         assertNotNull("Placeholder", null);
         */
-        String pass="Not a null string";
+        String pass=null;
         assertNull("VMworld 2016!", pass);
 
     }
